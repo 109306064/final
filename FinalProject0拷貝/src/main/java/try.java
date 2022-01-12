1d7c233e6ab6ee5ccc28cf7f82f8b3d7e1520ff2
ycyeha <74034816+ycyeha@users.noreply.github.com>
2022-01-13 00:29:50 +0800

    upload a class
    this is a class i wrote at the very beginning...

---
+++ FinalProject0拷貝/src/main/java/try.java
@@ -0,0 +1,11 @@
+
+public class CalcScore {
+//"extends WordCounter, KeywordList" to get count & weight ?? 
+	private double score;
+	
+	public double calculate() {
+		score = count*weight;
+		return score;
+	}
+
+}
