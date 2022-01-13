1b17faa76f88f696062289f7b088bc3df75776b3
Mei329 <maggie10135@gmail.com>
2022-01-13 09:05:01 +0800

    Rewrite the classes to calculate the score and sort

--- FinalProject/src/GoogleQuery.java
+++ FinalProject/src/GoogleQuery.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import java.io.InputStreamReader;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import java.net.URLConnection;
-import java.util.ArrayList;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 import org.jsoup.Jsoup;
@@ -21,13 +23,11 @@ import org.jsoup.nodes.Element;
 
 import org.jsoup.select.Elements;
 
-public class WebContent {
+public class GoogleQuery {
 	/**
 	 * To crawl down web content and identify specific info.
 	 */
-	private String content;
-
-	public WebContent() {
+	public GoogleQuery() {
 	}
 
 	public String fetchContent(String url) throws IOException {
@@ -35,55 +35,143 @@ public class WebContent {
 
 		try {
 			URL u = new URL(url);
-			
-			
-			URLConnection conn = u.openConnection();
-	
-			conn.setRequestProperty("User-agent", "Chrome/7.0.517.44");
-	
-			InputStream in = conn.getInputStream();
-	
-			InputStreamReader inReader = new InputStreamReader(in, "utf-8");
-	
-			BufferedReader bufReader = new BufferedReader(inReader);
-			String line = null;
-	
-			while ((line = bufReader.readLine()) != null) {
-				retVal += line;
-	
+
+			HttpURLConnection.setFollowRedirects(false);
+			HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+			conn.setRequestProperty("Accept", "*/*");
+			conn.setRequestProperty("Connection", "Keep-Alive");
+			conn.setRequestProperty("User-Agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0) Chrome/96.0.4664.110 Safari/15.2");
+			CookieHandler.setDefault(new CookieManager());
+
+			if (conn.getResponseCode() == 403 || conn.getResponseCode() == 400 || conn.getResponseCode() == 404) {
+				retVal = url;
+				System.out.printf("Error %d: %s\n", conn.getResponseCode(), retVal);
+			} else {
+				InputStream in = conn.getInputStream();
+				retVal = new String(in.readAllBytes(), StandardCharsets.UTF_8); // will fail with large amounts of data
 			}
-		
-		}catch (MalformedURLException e){
-			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			retVal = url;
+		} catch (IOException e) {
+			e.printStackTrace();
+			retVal = url;
 		}
-		
+
 		return retVal;
 	}
 
-	public HashMap<String, String> google(String url) throws IOException {
+	public String fetchGoogle(String url) throws IOException {
+		String retVal = "";
 
-		content = fetchContent(url);
+		try {
+			URL u = new URL(url);
+
+			HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+
+			conn.setRequestProperty("User-agent", "Chrome/96.0.4664.110");
+
+			InputStream in = conn.getInputStream();
+			InputStreamReader inReader = new InputStreamReader(in, "utf-8");
+
+			BufferedReader bufReader = new BufferedReader(inReader);
+
+			String line = null;
+			while ((line = bufReader.readLine()) != null) {
+				retVal += line;
+			}
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		return retVal;
+	}
+
+	public HashMap<String, String> query(String searchKeyword) throws IOException {
+		// format searchKeyword
+		String[] splitStr = searchKeyword.split(" ");
+		if (splitStr.length > 1) {
+			String comb = "";
+			for (int i = 0; i < splitStr.length; i++) {
+				comb += splitStr[i] + "+";
+			}
+			searchKeyword = comb;
+		}
+		searchKeyword.trim();
+
+		// to get results that are related to movie
+		String lang = "";
+		for (int i = 0; i < searchKeyword.length();) {
+			int codepoint = searchKeyword.codePointAt(i);
+			i += Character.charCount(codepoint);
+			if (Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.HAN) {
+				lang = "han";
+			} else if (Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.HIRAGANA
+					|| Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.KATAKANA) {
+				lang = "jp";
+			} else if (Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.HANGUL) {
+				lang = "kr";
+			}
+		}
+		if (lang.equals("han")) {
+			if (!searchKeyword.contains("電影")) {
+				searchKeyword += "+電影";
+			}
+		} else if (lang.equals("kr")) {
+			if (!searchKeyword.contains("영화")) {
+				searchKeyword += "+영화";
+			}
+		} else if (lang.equals("jp")) {
+			if (!searchKeyword.contains("えいが") || !searchKeyword.contains("映画")) {
+				searchKeyword += "+映画";
+			}
+		} else {
+			if (!searchKeyword.contains("movie")) {
+				searchKeyword += "+movie";
+			}
+		}
+
+		// search google
+		String url = "http://www.google.com/search?q=" + searchKeyword + "&oe=utf8&num=30";
+		System.out.println("url: " + url);
+
+		String content = fetchGoogle(url);
 		HashMap<String, String> retVal = new HashMap<String, String>();
 
 		Document doc = Jsoup.parse(content);
-
 		Elements lis = doc.select("div");
-
 		lis = lis.select(".kCrYT");
 
 		for (Element li : lis) {
 			try {
-				String citeUrl = li.select("a").attr("href");
+				// parse down URL link
+				String citeUrl = li.select("a").attr("href"); // System.out.println("origin: " + citeUrl);
+				if (citeUrl.startsWith("/url?q=")) {
+					citeUrl = citeUrl.replace("/url?q=", "");
+				}
+				String[] splittedString = citeUrl.split("&sa=");
+				if (splittedString.length > 1) {
+					citeUrl = splittedString[0];
+				}
+
+				// url decoding from UTF-8
+				citeUrl = java.net.URLDecoder.decode(citeUrl, StandardCharsets.UTF_8);
+				citeUrl.replaceAll(" ", "%20");
+
+				// parse down title
 				String title = li.select("a").select(".vvjwJb").text();
 				if (title.equals("")) {
 					continue;
 				}
-				System.out.println(title + "," + citeUrl);
+
+				System.out.println(title + ", " + citeUrl);
 				retVal.put(title, citeUrl);
 
 			} catch (IndexOutOfBoundsException e) {
 				e.printStackTrace();
-			} 
+			}
 		}
 		return retVal;
 	}
--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -1,11 +1,9 @@
 
-
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.List;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,52 +14,82 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class TestProject
  */
 @WebServlet("/TestProject")
-public class TestProject extends HttpServlet {
+public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestProject() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Main() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
-		if(request.getParameter("keyword")== null) {
+		if (request.getParameter("keyword") == null) {
 			String requestUri = request.getRequestURI();
 			request.setAttribute("requestUri", requestUri);
-			request.getRequestDispatcher("Main.jsp").forward(request, response);
+			request.getRequestDispatcher("Search.jsp").forward(request, response);
 			return;
 		}
-		WebTree google = new WebTree(request.getParameter("keyword"));
-		HashMap<String, String> query = google.search(content);
-		
+		System.out.println(request.getParameter("keyword"));
+
+		// search Google
+		GoogleQuery google = new GoogleQuery();
+		HashMap<String, String> query = google.query(request.getParameter("keyword"));
+		// prepare keyword list
+		KeywordList lst = new KeywordList();
+		String ctgry = lst.filter(request.getParameter("category"));
+		// throw google result and keyword list into CalcScore to calculate each webpage's score and sort
+		CalcScore calc = new CalcScore(query, lst);
+		HashMap<Integer, ArrayList<String>> score = calc.calcScore();
+		List<Integer> order = calc.sort(score);
+		// initiate a string array to send forward to googleitem.jsp
 		String[][] s = new String[query.size()][2];
 		request.setAttribute("query", s);
+
+		// parse HashMap into String[][] in order of [@order]
 		int num = 0;
-		for(Entry<String, String> entry : query.entrySet()) {
-		    String key = entry.getKey();
-		    String value = entry.getValue();
-		    s[num][0] = key;
-		    s[num][1] = value;
-		    num++;
+		for (Integer i : order) {
+			if (score.get(i).size() > 1) {
+				for (int j = 0; j < score.get(i).size(); j++) {
+					String key = score.get(i).get(j);
+					String value = query.get(key);
+					s[num][0] = key;
+					s[num][1] = value;
+					num++;
+				}
+			} else {
+				String key = score.get(i).get(0);
+				String value = query.get(key);
+				s[num][0] = key;
+				s[num][1] = value;
+				num++;
+			}
 		}
-		request.getRequestDispatcher("googleitem.jsp")
-		 .forward(request, response); 
 		
+		String[] info = { request.getParameter("keyword"), String.valueOf(query.size()), ctgry };
+		request.setAttribute("info", info);
+		
+		request.getRequestDispatcher("ResultPage.jsp").forward(request, response);
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
